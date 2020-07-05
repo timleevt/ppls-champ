a1b100245cde32467dce363838da788f69f532e3
timleevt <timleemu@vt.edu>
2020-07-04 22:10:11 -0400

    trying to figure out why build is not changing

---
+++ client/build/precache-manifest.5a39dd62e58c625b529b2cd2f7a6de5f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed27cf835e9b25af4ef82cba5695fad5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b7d414186bce94ba69b",
+    "url": "/static/css/2.0c0672e5.chunk.css"
+  },
+  {
+    "revision": "56f567fc428385c13de2",
+    "url": "/static/css/main.0a4ab30b.chunk.css"
+  },
+  {
+    "revision": "3b7d414186bce94ba69b",
+    "url": "/static/js/2.8edf9444.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8edf9444.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56f567fc428385c13de2",
+    "url": "/static/js/main.1c2a0a47.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "05a54d2b64caa05964e30f47c2587cd2",
+    "url": "/static/media/banner3.05a54d2b.jpeg"
+  }
+]);
